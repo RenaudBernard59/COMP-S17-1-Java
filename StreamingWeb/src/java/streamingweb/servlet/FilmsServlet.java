101cab5f7ea040abc23adb9d28d19acc66cceea6
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-18 09:58:28 +0200

    Edit FilmServlet
    Add Code to servlet.

--- StreamingWeb/src/java/streamingweb/servlet/FilmsServlet.java
+++ StreamingWeb/src/java/streamingweb/servlet/FilmsServlet.java
@@ -7,11 +7,14 @@ package streamingweb.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import streamingweb.service.FilmService;
+import streamingweb.service.FilmServiceImpl;
 
 /**
  *
@@ -19,10 +22,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "FilmsServlet", urlPatterns = {"/films"})
 public class FilmsServlet extends HttpServlet {
-
+    private FilmService service = new FilmServiceImpl();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1. Récupérer la liste de films avec FilmService
+        List<Film> films = service.listerFilms();
         
+        
+        //2. Envoyer liste films film comme attribut request
+        
+        //3. Envoyer vers films imp
         
     }
     
