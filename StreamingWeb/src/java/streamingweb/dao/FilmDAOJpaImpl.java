0503a02b7d97112d7219aebc27417b8cee2be743
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-18 09:26:35 +0200

    Add DAO
    Addinf new DAO JPA Implementation.

---
+++ StreamingWeb/src/java/streamingweb/dao/FilmDAOJpaImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streamingweb.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import streamingweb.entity.Film;
+
+/**
+ *
+ * @author renob
+ */
+public class FilmDAOJpaImpl implements FilmDAO {
+
+    @Override
+    public List<Film> lister() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query q = em.createQuery("SELECT f FROM Film f");
+        List<Film> films = q.getResultList();
+        return films;
+    }
+    
+}
